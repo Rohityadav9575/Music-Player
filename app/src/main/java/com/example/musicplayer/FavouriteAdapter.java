1dbce3b96723fb1ccd24de35d2a2d6183e0fcec3
Rohityadav9575 <ry651621@gmail.com>
2024-09-27 15:11:17 +0530

    image of song is visible in favourite fragment and songs can we played from favourite tab also

--- app/src/main/java/com/example/musicplayer/FavouriteAdapter.java
+++ app/src/main/java/com/example/musicplayer/FavouriteAdapter.java
@@ -1,6 +1,9 @@
 package com.example.musicplayer;
 
 import android.content.Context;
+import android.content.Intent;
+import android.media.MediaMetadataRetriever;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +13,9 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
+import java.io.IOException;
 import java.util.List;
 
 public class FavouriteAdapter extends RecyclerView.Adapter<FavouriteAdapter.ViewHolder> {
@@ -36,6 +42,36 @@ public class FavouriteAdapter extends RecyclerView.Adapter<FavouriteAdapter.View
         holder.song_name.setText(song.getName()); // Assuming you have a getTitle() method
 
         holder.songimage.setImageResource(R.drawable.default_music); // Placeholder image
+
+
+
+        byte[] image = null;
+        try {
+            image = getAlbumArt(favouriteList.get(position).getPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        if(image!=null) {
+            Glide.with(context)
+                    .asBitmap()
+                    .load(image)// URL of the image
+                    .into(holder.songimage); // Target ImageView
+        }else{
+            Glide.with(context)
+                    .load(R.drawable.default_music)
+                    .into(holder.songimage);
+        }
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(context,PlayingActivity.class);
+                intent.putExtra("Position",holder.getAdapterPosition());
+                context.startActivity(intent);
+
+            }
+        });
+
     }
 
     @Override
@@ -58,6 +94,19 @@ public class FavouriteAdapter extends RecyclerView.Adapter<FavouriteAdapter.View
             songimage = itemView.findViewById(R.id.imageViewMusicCover);
             song_name = itemView.findViewById(R.id.textViewMusicTitle);
             song_artist = itemView.findViewById(R.id.textViewMusicArtist);
+        }
+    }
+    private byte[] getAlbumArt(String uri) throws IOException {
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        try {
+            retriever.setDataSource(uri);
+            return retriever.getEmbeddedPicture();
+        } catch (Exception e) {
+            // Log the exception instead of throwing it
+            Log.e("MediaMetadata", "Error retrieving album art", e);
+            return null;
+        } finally {
+            retriever.release();
         }
     }
 }
